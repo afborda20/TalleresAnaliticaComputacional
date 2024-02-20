70c13a8bf1b42cdd7da681bc35f09e168c67058e
afborda20 <af.borda@uniandes.edu.co>
2024-02-19 19:03:04 -0500

    App3 modificados

---
+++ Taller4/app3modified.py
@@ -0,0 +1,80 @@
+import dash
+from dash import dcc  # dash core components
+from dash import html # dash html components
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminderDataFiveYear.csv')
+print(df.columns)
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.layout = html.Div([
+    dcc.Graph(id='graph-with-slider'),
+    dcc.Slider(
+        id='year-slider',
+        min=df['year'].min(),
+        max=df['year'].max(),
+        value=df['year'].min(),
+        marks={str(year): str(year) for year in df['year'].unique()},
+        step=None
+    ),
+    html.Hr(),
+   dcc.Dropdown(
+    id="country",
+    options=[{'label': country, 'value': country} for country in df['country'].unique()],
+    value=df['country'].unique()[0],  
+    multi=False
+    ),
+    dcc.Graph(id='line-graph')
+    
+
+])
+
+
+@app.callback(
+    Output('graph-with-slider', 'figure'),
+    [Input('year-slider', 'value')])
+def update_figure(selected_year):
+    filtered_df = df[df.year == selected_year]
+
+    fig = px.scatter(filtered_df, x="gdpPercap", y="lifeExp", 
+                     size="pop", color="continent", hover_name="country", 
+                     log_x=True, size_max=55,
+                     labels={
+                     "pop": "Population",
+                     "gdpPercap": "GDP per cápita",
+                     "lifeExp": "Life Expectancy",
+                     "continent": "Continent"
+                     },
+                     title="Life expectancy vs. GDP per cápita across the years")
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+@app.callback(
+    Output('line-graph', 'figure'),
+    [Input('country', 'value')])
+def update_figure(selected_country):
+    filtered_df = df[df.country == selected_country]
+
+    fig = px.line(filtered_df, x="year", y="gdpPercap",
+                    labels={"year": "Year",  "gdpPercap": "GDP per cápita",},
+                    line_group="country",
+                    hover_name="year",
+                    line_shape="spline",
+                    render_mode="svg",
+                    title="GDP over the years: "+ selected_country)
+
+    fig.update_layout(transition_duration=500)
+
+    return fig
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
